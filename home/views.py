b4fc626c7c3a4adea7b12ca73092b7089e788cbe
AriqRashid <az.rashid74@gmail.com>
2021-01-07 18:41:53 -0500

    Can now Upload images from home page

--- home/views.py
+++ home/views.py
@@ -1,21 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from django.template import loader
+from .models import Image
 from .forms import ImageForm
 # Create your views here.
 def index(request):
 	context = {}
 	return render(request, 'home/templates/index.html', context)
 
-def image_upload_view(request):
-    """Process images uploaded by users"""
+
+def image_view(request):
     if request.method == 'POST':
         form = ImageForm(request.POST, request.FILES)
+
         if form.is_valid():
             form.save()
-            # Get the current instance object to display in the template
-            img_obj = form.instance
-            return render(request, 'index.html', {'form': form, 'img_obj': img_obj})
+
     else:
-        form = ImageForm()
+        form = Image()
     return render(request, 'index.html', {'form': form})
+
+
--- project_management_root/urls.py
+++ project_management_root/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from home import views
 
 urlpatterns = [
 	path('', include('home.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        urlpatterns += static(settings.MEDIA_URL,
+                              document_root=settings.MEDIA_ROOT)
